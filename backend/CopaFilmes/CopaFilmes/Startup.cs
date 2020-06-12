3bdf6276719e2ee328bac5307b9be8d8bd25aca6
Cristian <csanterio@gmail.com>
2020-06-12 06:38:22 -0300

    configurações de deploy

--- backend/CopaFilmes/CopaFilmes/Startup.cs
+++ backend/CopaFilmes/CopaFilmes/Startup.cs
@@ -43,10 +43,8 @@ namespace CopaFilmes
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            if (!env.IsDevelopment())
+                app.UseHsts();
 
             app.UseHttpsRedirection();
 
